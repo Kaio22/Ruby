65579a29df3f6b2ac2ecd74c7e8f53d28e2f5e43
kaio22 <kaio.22.nascimento@gmail.com>
2017-09-13 17:36:18 -0300

    Novos arquivos

--- rest-api/application.rb
+++ rest-api/application.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
 
+set :port, 9090
+# olhar o set :bind, 0.0.0.0
+
 get '/' do
-   "minha aplicacao"
+   erb :index
+end
+
+
+
+#get '/cacilds' do
+#File.read("/tmp/resolv.conf")
+#end
+
+post '/login' do 
+  usuarios = []
+  usuarios.push(params)
+  puts usuarios.inspect
 end
